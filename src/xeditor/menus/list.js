fa603f71e9bc2377ac0cf868b87f98bc2dd1279a
李梦龙 <649076408@qq.com>
2017-11-17 16:53:17 +0800

    feature: add justifyleft

--- src/xeditor/menus/list.js
+++ src/xeditor/menus/list.js
@@ -2,6 +2,7 @@ import bold from './bold';
 import italic from './italic';
 import justifycenter from './justifycenter';
 import justifyfull from './justifyfull';
+import justifyleft from './justifyleft';
 
 const list = {};
 
@@ -9,5 +10,6 @@ list.bold = bold;
 list.italic = italic;
 list.justifycenter = justifycenter;
 list.justifyfull = justifyfull;
+list.justifyleft = justifyleft;
 
 export default list;

69a46d0af11314ad65ed362af01ddbc47d943b22
李梦龙 <649076408@qq.com>
2017-11-17 16:23:44 +0800

    feature: add justifycenter

--- src/xeditor/menus/list.js
+++ src/xeditor/menus/list.js
@@ -1,9 +1,11 @@
 import bold from './bold';
 import italic from './italic';
+import justifycenter from './justifycenter';
 
 const list = {};
 
 list.bold = bold;
 list.italic = italic;
+list.justifycenter = justifycenter;
 
 export default list;

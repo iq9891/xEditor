8722e0d10da8a72a8d669013c9e916b0e8712506
李梦龙 <649076408@qq.com>
2018-01-12 20:38:42 +0800

    feature: create site

--- config/prod.env.js
+++
@@ -1,6 +0,0 @@
-var constant = require('./constant.env')
-
-module.exports = {
-  NODE_ENV: '"production"',
-  API: constant.API_PRODUCTION,
-}
--- src/xeditor/index.js
+++
@@ -1,8 +0,0 @@
-// XEditor 主类
-import XEditor from './editor';
-// XEditor 样式
-import '../assets/styles/editor.scss';
-
-if (typeof window !== 'undefined' && !window.xEditor) {
-  window.xEditor = XEditor;
-}

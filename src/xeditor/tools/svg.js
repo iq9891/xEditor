d93884269aed69828cffe0dfbe7adddc697f06db
李梦龙 <649076408@qq.com>
2018-05-18 18:10:18 +0800

    fix($tip): suspended above tips

--- src/xeditor/tools/svg.js
+++ src/xeditor/tools/svg.js
@@ -16,7 +16,7 @@ function svg($parent, type) {
 
   $parent.html(`
     <svg width="16px" height="16px" viewbox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <title>backcolor</title>
+      <title>${cfg.lang[type]}</title>
       <g stroke="none" stroke-width="1" fill="#999999" fill-rule="nonzero">
         ${pathTem}
       </g>
